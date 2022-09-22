b5e1c6f6d144e8c425b48402a9d059066a293570
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-09-22 15:21:08 +0100

    add react and test

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (<>
+    <div>Navbar</div>
+    <a href="#contacs" className="contats">Contacts</a>
+    </>
+  )
+}
+
+export default Navbar
