ceb47b9a1607ba303d0cc451cb09c4f5628e825c
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-09-22 17:31:36 +0100

    add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "../../Styles/sass/main.css";
+import logo from "../../Assets/Images/logo.png";
 
 function Navbar() {
 	return (
 		<div className="navbar">
+
 			{/* Top section of Navbar */}
-			<div className="navbar__top"></div>
+			<div className="navbar__top">
+				<div className="navbar__top--logo-container">
+					<a href="#netmatters" className="navbar__top--logo-container--logo">
+						<img src="logo" alt="" />
+					</a>
+				</div>
+			</div>
+
 			{/* Bottom section of Navbar */}
-			<div className="navbar__bottom"></div>
+			<div className="navbar__bottom">
+
+			</div>
 		</div>
 	);
 }
