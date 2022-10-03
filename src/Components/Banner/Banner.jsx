3f10f817578bcc02a10e5f32fac16a2b34569a49
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-10-03 15:17:54 +0100

    add a banner, NOT COMPLETE

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import BannerImg from '../../Assets/Images/home-banner.webp'
+import BannerImg from "../../Assets/Images/home-banner.webp";
 
 function Banner() {
 	return (
 		<div className="banner-container">
 			<div className="banner-container__row">
 				<div className="banner-container__row--banner">
-				<div className="banner-container__row--banner--image">
-                <img
-						src={BannerImg}
-						alt="Netmatter banner"
-					/>
-                </div>
+					<div className="banner-container__row--banner--image">
+						<img src={BannerImg} alt="Netmatter banner" />
+					</div>
 					<div className="banner-container__row--banner--text">
-						<h2>The East Of England's Leading</h2>
-						<p>
-							Performance-driven digital and technology services
-							<br />
-							with complete transparency.
-						</p>
+						<div className="banner-container__row--banner--text--h2">
+							<h2>The East Of England's Leading Technology Company</h2>
+						</div>
+						<div className="banner-container__row--banner-text--h2--paragraph">
+							<p>
+								Performance-driven digital and technology services
+								<br />
+								with complete transparency.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
