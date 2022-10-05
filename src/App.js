75c3fb6bb1f7301f0665835d8826f2f16c645291
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-10-05 14:17:37 +0100

    add a non working awards component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar';
 import NavbarTwo from './Components/Navbar/NavbarTwo';
 import Banner from './Components/Banner/Banner'
 import OurServices from './Components/OurServices/OurServices';
+import Awards from './Components/Awards/Awards';
 // import SideMenu from './Components/SideMenu/SideMenu';
 
 
@@ -16,6 +17,7 @@ function App() {
       {/* <SideMenu /> */}
       <Banner />
       <OurServices />
+      <Awards />
       <Footer />
     </div>
   );
---
+++ src/Components/Awards/Awards.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import img1a from '../../Assets/Images/award-img1a.jpg'
+import img1b from '../../Assets/Images/award-img1b.jpg'
+import img2a from '../../Assets/Images/award-img2a.jpg'
+import img2b from '../../Assets/Images/award-img2b.jpg'
+import img3a from '../../Assets/Images/award-img3a.jpg'
+import img3b from '../../Assets/Images/award-img3b.jpg'
+import img4a from '../../Assets/Images/award-img4a.jpg'
+import img4b from '../../Assets/Images/award-img4b.jpg'
+import img5a from '../../Assets/Images/award-img5a.jpg'
+import img5b from '../../Assets/Images/award-img5b.jpg'
+import img6a from '../../Assets/Images/award-img6a.jpg'
+import img6b from '../../Assets/Images/award-img6b.jpg'
+import img7a from '../../Assets/Images/award-img7a.jpg'
+import img7b from '../../Assets/Images/award-img7b.jpg'
+import img8a from '../../Assets/Images/award-img8a.jpg'
+import img8b from '../../Assets/Images/award-img8b.jpg'
+import img9a from '../../Assets/Images/award-img9a.jpg'
+import img9b from '../../Assets/Images/award-img9b.jpg'
+import img10a from '../../Assets/Images/award-img10a.jpg'
+import img10b from '../../Assets/Images/award-img10b.jpg'
+import img11a from '../../Assets/Images/award-img11a.jpg'
+import img11b from '../../Assets/Images/award-img11b.jpg'
+import img12a from '../../Assets/Images/award-img12a.jpg'
+import img12b from '../../Assets/Images/award-img12b.jpg'
+import img13a from '../../Assets/Images/award-img13a.jpg'
+import img13b from '../../Assets/Images/award-img13b.jpg'
+
+function Awards() {
+  return (
+    <section className="section__awards">
+        {/* Start of image 1 */}
+        <div className="section__awards--image-container">
+            <img src={img1a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img1b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 1 */}
+
+        {/* Start of image 2 */}
+        <div className="section__awards--image-container">
+            <img src={img2a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img2b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 2 */}
+
+        {/* Start of image 3 */}
+        <div className="section__awards--image-container">
+            <img src={img3a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img3b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 3 */}
+
+        {/* Start of image 4 */}
+        <div className="section__awards--image-container">
+            <img src={img4a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img4b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 4 */}
+
+        {/* Start of image 5 */}
+        <div className="section__awards--image-container">
+            <img src={img5a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img5b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 5 */}
+
+        {/* Start of image 6 */}
+        <div className="section__awards--image-container">
+            <img src={img6a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img6b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 6 */}
+
+        {/* Start of image 7 */}
+        <div className="section__awards--image-container">
+            <img src={img7a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img7b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 7 */}
+
+        {/* Start of image 8 */}
+        <div className="section__awards--image-container">
+            <img src={img8a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img8b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 8 */}
+
+        {/* Start of image 9 */}
+        <div className="section__awards--image-container">
+            <img src={img9a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img9b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 9 */}
+
+        {/* Start of image 10 */}
+        <div className="section__awards--image-container">
+            <img src={img10a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img10b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 10 */}
+
+        {/* Start of image 11 */}
+        <div className="section__awards--image-container">
+            <img src={img11a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img11b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 11 */}
+
+        {/* Start of image 12 */}
+        <div className="section__awards--image-container">
+            <img src={img12a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img12b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 12 */}
+
+        {/* Start of image 13 */}
+        <div className="section__awards--image-container">
+            <img src={img13a} alt="award" className="section__awards--image-container--imageOne"/>
+            <img src={img13b} alt="award" className="section__awards--image-container--imageTwo"/>
+        </div>
+        {/* End of image 13 */}
+
+
+
+
+    </section>
+  )
+}
+
+export default Awards
