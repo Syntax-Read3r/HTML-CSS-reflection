fb2cda2eae29f1b5da61933f2a74e1057660cf62
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-10-06 18:04:29 +0100

    add reviews component

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -6,71 +6,72 @@ import { BsFillStarFill } from "react-icons/bs";
 function Reviews() {
 	return (
 		<section className="reviews__section">
-			{/* Beginning of column one */}
-			<div className="reviews__section--columnOne">
-				<h2 className="reviews__section--columnOne--title">
-					Welcome To Netmatters
-				</h2>
-				<p className="review__section--columnOne--paragraph">
-					Netmatters is a leading Bespoke Software, IT Support, and Digital
-					Marketing company based in the East of England with offices in
-					Cambridge, Wymondham, and Great Yarmouth. We aren't tied into
-					contracts with third-party providers, so you know that our
-					recommendations for your business are based purely with one benefit in
-					mind: to help improve your business with the most appropriate
-					solutions. We pride ourselves on being an ethical business and have a
-					unique business offering and cost model that ensures you get the most
-					from our relationship in an upfront manner.
-				</p>
+			<div className="reviews__section--container">
+				{/* Beginning of column one */}
+				<div className="reviews__section--container--columnOne">
+					<h2 className="reviews__section--container--columnOne--title">
+						Welcome To Netmatters
+					</h2>
+					<p className="reviews__section--container--columnOne--paragraph">
+						Netmatters is a leading Bespoke Software, IT Support, and Digital
+						Marketing company based in the East of England with offices in
+						Cambridge, Wymondham, and Great Yarmouth.
+					</p>
 
-				<Button className="reviews__section--columnOne--btn">
-					Read More{" "}
-					<IoMdArrowRoundForward className="reviews__section--columnOne--btn--icon" />
-				</Button>
-			</div>
-			{/* End of column one */}
+					<p className="reviews__section--container--columnOne--paragraph">We aren't tied into contracts with third-party providers, so you know that our recommendations for your business are based purely with one benefit in mind: to help improve your business with the most appropriate solutions.</p>
 
-			{/* Begining of column two */}
-			<div className="reviews__section--columnTwo">
-        {/* Title */}
-				<h2 className="reviews__section--columnTwo--title">
-					What Our Clients Think
-				</h2>
-        {/* End of Title */}
+					<p className="reviews__section--container--columnOne--paragraph">We pride ourselves on being an ethical business and have a unique business offering and cost model that ensures you get the most from our relationship in an upfront manner.</p>
 
-        {/* Beginning of reviews */}
-				<div className="review__section--columnTwo--stars">
-					<BsFillStarFill className="review__section--columnTwo--stars--icon" />
-					<BsFillStarFill className="review__section--columnTwo--stars--icon" />
-					<BsFillStarFill className="review__section--columnTwo--stars--icon" />
-					<BsFillStarFill className="review__section--columnTwo--stars--icon" />
-					<BsFillStarFill className="review__section--columnTwo--stars--icon" />
-				</div>
-        
+					{/* <p className="reviews__section--container--columnOne--paragraph"></p> */}
 
-				<p className="review__section--columnTwo--paragraph">
-					Great service. Took the reigns and did all the leg work for us.
-					Ensured we were happy with our website and made all changes necessary
-					as and when asked to do so.
-				</p>
-
-				<p className="review__section--columnTwo-author">
-					Oliver King - SEA Scaffolding
-				</p>
-
-				<div className="review__section--columnTwo--btn--container">
-					{" "}
-					<Button className="reviews__section--columnTwo--btn--container--btnOne">
-						Google Reviews{" "}
-						<IoMdArrowRoundForward className="reviews__section--columnTwo--btn--container--btnOne--icon" />
+					<Button className="reviews__section--container--columnOne--btn">
+						Read More{" "}
+						<IoMdArrowRoundForward className="reviews__section--container--columnOne--btn--icon" />
 					</Button>
-          <Button className="reviews__section--columnTwo--btn--container--btnTwo">
-            Trustpilot Reviews{" "}
-            <IoMdArrowRoundForward className="reviews__section--columnTwo--btn--container--btnTwo--icon" />
-          </Button>
 				</div>
+				{/* End of column one */}
+
+				{/* Begining of column two */}
+				<div className="reviews__section--container--columnTwo">
+					{/* Title */}
+					<h2 className="reviews__section--container--columnTwo--title">
+						What Our Clients Think
+					</h2>
+					{/* End of Title */}
+
+					{/* Beginning of reviews */}
+					<div className="reviews__section--container--columnTwo--stars">
+						<BsFillStarFill className="reviews__section--container--columnTwo--stars--icon" />
+						<BsFillStarFill className="reviews__section--container--columnTwo--stars--icon" />
+						<BsFillStarFill className="reviews__section--container--columnTwo--stars--icon" />
+						<BsFillStarFill className="reviews__section--container--columnTwo--stars--icon" />
+						<BsFillStarFill className="reviews__section--container--columnTwo--stars--icon" />
+					</div>
+
+					<p className="reviews__section--container--columnTwo--paragraph">
+						Great service. Took the reigns and did all the leg work for us.
+						Ensured we were happy with our website and made all changes
+						necessary as and when asked to do so.
+					</p>
+
+					<p className="reviews__section--container--columnTwo--author">
+						Oliver King - SEA Scaffolding
+					</p>
+
+					<div className="reviews__section--container--columnTwo--btn-container">
+						{" "}
+						<Button className="reviews__section--container--columnTwo--btn-container--btnOne">
+							Google Reviews{" "}
+							<IoMdArrowRoundForward className="reviews__section--container--columnTwo--btn--container--btnOne--icon" />
+						</Button>
+						<Button className="reviews__section--container--columnTwo--btn-container--btnTwo">
+							Trustpilot Reviews{" "}
+							<IoMdArrowRoundForward className="reviews__section--container--columnTwo--btn--container--btnTwo--icon" />
+						</Button>
+					</div>
+				</div>
+				{/* End of column two */}
 			</div>
-			{/* End of column two */}
 		</section>
 	);
 }
