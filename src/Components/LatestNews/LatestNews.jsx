5c0f9fd178a68131ec1c4883459cf48b91e9f8ff
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-10-06 20:19:27 +0100

    add a complete latestnews header

--- src/Components/LatestNews/LatestNews.jsx
+++ src/Components/LatestNews/LatestNews.jsx
@@ -5,8 +5,13 @@ function LatestNews() {
 	return (
 		<div className="latestNews__container">
 			<div className="latestNews__container--header">
-				<h2>Latest News</h2>
-				<h3>View All <IoMdArrowRoundForward className="latestNews__container--header--icon"/></h3>
+				<h2 className="latestNews__container--header--h2">Latest News</h2>
+				<div className="latestNews__container--header--h3-container">
+					<h3>
+						View All
+					</h3>
+					<IoMdArrowRoundForward className="latestNews__container--header--h3-container--icon" />
+				</div>
 			</div>
 			<div className="latestNews__container--news"></div>
 		</div>
